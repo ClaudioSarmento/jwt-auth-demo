cb1206ad044bbfcd8e4458d5df97564059bcafe5
ClaudioSarmento <claudio-s-i@hotmail.com>
2025-04-14 21:05:55 -0300

    feature - Add SwaggerDoc, SecurityDefinition e SecurityRequirement

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using JwtApi.Repositories.Interfaces;
 using JwtApi.Services;
 using JwtApi.Services.Interfaces;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.OpenApi.Models;
 using Raven.Client.Documents;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,14 +13,48 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo()
+    {
+        Version = "v1",
+        Title = "Admin.Api",
+        Description = "Api de Admin"
+    });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Cabeçalho de Autorização JWT está usando o esquema de Bearer \r\n\r\n Digite 'Bearer' antes de colocar o Token"
+
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+            }
+        },
+        Array.Empty<string>()
+        }
+    });
+});
 builder.Services.TryAddScoped<IUserRepository, UserRepository>();
 builder.Services.TryAddScoped<IUserService, UserService>();
 builder.Services.TryAddScoped<ITokenService, TokenService>();
 
 var documentStore = new DocumentStore
 {
-    Urls = new[] { "http://localhost:8080" } ,
+    Urls = new[] { "http://localhost:8080" },
     Database = "JwtDataBase"
 }.Initialize();
 
